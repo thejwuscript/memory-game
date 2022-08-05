f3296baf04e464f951551a312abfb6535ad5b906
James Wu <ichangjwu@outlook.com>
2022-08-06 08:22:12 +0900

    Add images and decide card layout

---
+++ src/components/Card.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../styles/card.css';
+
+function Card() {
+ return <div className="card"></div>
+};
+
+export default Card;
---
+++ src/components/Main.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Card from './Card';
+import '../styles/main.css';
+
+function Main() {
+  return (
+    <main>
+      <div className="cards-container">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </main>
+  )
+};
+
+export default Main;
